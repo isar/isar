099f49688acf80333d58b761c02fcfce2492b3dc
Simon Leier <simonleier@gmail.com>
2020-12-15 19:18:20 +0100

    Add isar_annotation package

---
+++ isar_annotation/README.md
@@ -0,0 +1,3 @@
+# Isar annotation
+
+Annotations for the Isar database. You don't need to manually add this package.
---
+++ isar_annotation/lib/isar_annotation.dart
@@ -0,0 +1,31 @@
+class Collection {
+  const Collection();
+}
+
+class Ignore {
+  const Ignore();
+}
+
+class Index {
+  final List<String> composite;
+
+  final bool unique;
+
+  final bool hashValue;
+
+  const Index({
+    this.composite = const [],
+    this.unique = true,
+    this.hashValue = false,
+  });
+}
+
+class Name {
+  final String name;
+
+  const Name(this.name);
+}
+
+class Size32 {
+  const Size32();
+}
