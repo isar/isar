ce5401ddd8e1d24fff46d1ffcf917882a8bbfbc9
Simon Leier <simonleier@gmail.com>
2021-01-06 19:47:42 +0100

    Fix bindings generation

--- isar/tools/fix_bindings.dart
+++ isar/tools/fix_bindings.dart
@@ -4,11 +4,12 @@ void main() {
   final file = File('lib/src/native/bindings.dart');
   var contents = file.readAsStringSync();
   contents = contents.replaceAll(
-    RegExp(r'ffi\.Pointer<(?!(ffi|IsarCoreBindings|RawObject))[^>]+>'),
+    RegExp(
+        r'ffi\.Pointer<(?!(ffi|IsarCoreBindings|RawObject|Dart_CObject))[^>]+>'),
     'ffi.Pointer<ffi.NativeType>',
   );
   contents = contents.replaceAll(
-    RegExp(r'class (?!(IsarCoreBindings|RawObject))[^}]+}'),
+    RegExp(r'class (?!(IsarCoreBindings|RawObject|Dart_CObject))[^}]+}'),
     '',
   );
   file.writeAsStringSync(contents);
