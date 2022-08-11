a3443f9f23c92a366417f70c4ca87a73cd273d6a
Simon Leier <simonleier@gmail.com>
2022-08-12 00:46:25 +0200

    Update version.dart

--- packages/isar/lib/src/version.dart
+++ packages/isar/lib/src/version.dart
@@ -2,7 +2,7 @@
 
 import 'dart:math';
 
-const isarCoreVersion = '2.5.68';
+const isarCoreVersion = '2.5.69';
 final isarCoreVersionNumber = _getVersionNumber(isarCoreVersion);
 
 const isarWebVersion = '2.5.1';
