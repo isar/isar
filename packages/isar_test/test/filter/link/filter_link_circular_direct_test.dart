fb7bba6473c5c24de2170c87da54fd2e5c694174
Jtplouffe <32107801+Jtplouffe@users.noreply.github.com>
2022-08-13 21:53:33 -0400

    Started link filter refactor

---
+++ packages/isar_test/test/filter/link/filter_link_circular_direct_test.dart
@@ -0,0 +1,8 @@
+import 'package:isar/isar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Filter link circular direct', () {
+    late Isar isar;
+  });
+}
---
+++ packages/isar_test/test/filter/link/filter_link_multiple_test.dart
@@ -0,0 +1,8 @@
+import 'package:isar/isar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Filter link multiple', () {
+    late Isar isar;
+  });
+}
