02da9ca8fbe01f8fc18bb264dc892f61dcbe014a
Simon Leier <simonleier@gmail.com>
2022-09-23 15:09:15 +0200

    Update isar.dart

--- packages/isar/lib/src/isar.dart
+++ packages/isar/lib/src/isar.dart
@@ -18,7 +18,7 @@ abstract class Isar {
   }
 
   /// The version of the Isar library.
-  static const version = '3.0.0';
+  static const version = '3.0.1';
 
   /// Smallest valid id.
   static const Id minId = isarMinId;
