2879772e2d2852a312a4b8803de4ce62ede524c6
ndelanou <nicolas.delanou@shipfix.com>
2022-10-25 22:02:13 +0200

    Fix collection size calculation in inspector

--- packages/isar_inspector/lib/collections_list.dart
+++ packages/isar_inspector/lib/collections_list.dart
@@ -92,6 +92,6 @@ class CollectionsList extends StatelessWidget {
 String _formatSize(int bytes) {
   if (bytes <= 0) return '0 B';
   const suffixes = ['B', 'KB', 'MB', 'GB'];
-  final i = (log(bytes) / log(1000)).floor();
-  return '${(bytes / pow(1000, i)).toStringAsFixed(2)} ${suffixes[i]}';
+  final i = (log(bytes) / log(1024)).floor();
+  return '${(bytes / pow(1024, i)).toStringAsFixed(2)} ${suffixes[i]}';
 }
