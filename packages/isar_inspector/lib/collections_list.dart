5a142e2d982684caa4e747a9bb3fcff8a7ff5e16
ndelanou <nicolas.delanou@shipfix.com>
2022-10-25 22:24:36 +0200

    Remove floating numbers when less than 1KB

--- packages/isar_inspector/lib/collections_list.dart
+++ packages/isar_inspector/lib/collections_list.dart
@@ -93,5 +93,5 @@ String _formatSize(int bytes) {
   if (bytes <= 0) return '0 B';
   const suffixes = ['B', 'KB', 'MB', 'GB'];
   final i = (log(bytes) / log(1024)).floor();
-  return '${(bytes / pow(1024, i)).toStringAsFixed(2)} ${suffixes[i]}';
+  return '${(bytes / pow(1024, i)).toStringAsFixed(i == 0 ? 0 : 2)} ${suffixes[i]}';
 }
