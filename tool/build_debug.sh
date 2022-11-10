b0e3cddbb2f2a55b62118e6bdf356fcd1b3740b6
Simon Leier <simonleier@gmail.com>
2022-11-10 08:16:52 +0100

    Update build_debug.sh

--- tool/build_debug.sh
+++ tool/build_debug.sh
@@ -7,15 +7,15 @@ case "$OSTYPE" in
     else
         cargo build --target aarch64-apple-darwin
     fi
-  ;; 
-  linux*)
+    ;; 
+  linux-gnu*)
     if [[ $arch == x86_64* ]]; then
         cargo build --target x86_64-unknown-linux-gnu
     else
         cargo build --target aarch64-unknown-linux-gnu
     fi
-  ;;
+    ;;
   *)
     cargo build --target x86_64-pc-windows-msvc
-  ;;
+    ;;
 esac
