5cd1efa3a9003af54b8f873de434a32838bf914d
Simon Leier <simonleier@gmail.com>
2022-11-10 08:01:39 +0100

    Fixes

--- packages/isar_test/integration_test/integration_test.dart
+++ packages/isar_test/integration_test/integration_test.dart
@@ -1,10 +1,5 @@
 // ignore_for_file: avoid_print
 
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:isar_test/isar_test.dart';
 import 'package:path_provider/path_provider.dart';
 
--- tool/build_debug.sh
+++ tool/build_debug.sh
@@ -1,16 +1,21 @@
 arch=$(uname -m)
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if [[ $arch == x86_64* ]]; then
-        cargo build --target x86_64-unknown-linux-gnu
-    else
-        cargo build --target aarch64-unknown-linux-gnu
-    fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+case "$OSTYPE" in
+  darwin*)
     if [[ $arch == x86_64* ]]; then
         cargo build --target x86_64-apple-darwin
     else
         cargo build --target aarch64-apple-darwin
     fi
-else
+  ;; 
+  linux*)
+    if [[ $arch == x86_64* ]]; then
+        cargo build --target x86_64-unknown-linux-gnu
+    else
+        cargo build --target aarch64-unknown-linux-gnu
+    fi
+  ;;
+  *)
     cargo build --target x86_64-pc-windows-msvc
-fi
+  ;;
+esac
