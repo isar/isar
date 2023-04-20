d651c82b18ad6c99f1598fe2242663e6a039d72d
Simon Leier <simonleier@gmail.com>
2023-04-20 12:18:46 +0900

    Remove old files

--- packages/isar_core/src/lib.rs
+++ packages/isar_core/src/lib.rs
@@ -3,20 +3,6 @@
 #[cfg(not(target_endian = "little"))]
 compile_error!("Only little endian systems are supported.");
 
-/*pub mod collection;
-mod cursor;
-
-pub mod index;
-pub mod instance;
-mod legacy;
-mod link;
-mod mdbx;
-
-pub mod query;
-pub mod schema;
-pub mod txn;
-pub mod watch;*/
-
 pub mod common;
 pub mod core;
 pub mod native;
