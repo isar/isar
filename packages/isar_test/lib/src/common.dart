d4b3a1e16cdc92e6c4e4679a237a3f07b1ff78f2
Simon Choi <13610195+simc@users.noreply.github.com>
2023-07-21 21:11:16 +0200

    Fixes

--- packages/isar_test/lib/src/common.dart
+++ packages/isar_test/lib/src/common.dart
@@ -23,46 +23,47 @@ void prepareTest() {
           : Platform.isMacOS
               ? 'libisar.dylib'
               : 'libisar.so';
-
-      Isar.initializeIsarCore(
-        libraries: {
-          Abi.macosArm64: path.join(
-            rootDir,
-            'target',
-            'aarch64-apple-darwin',
-            'release',
-            binaryName,
-          ),
-          Abi.macosX64: path.join(
-            rootDir,
-            'target',
-            'x86_64-apple-darwin',
-            'release',
-            binaryName,
-          ),
-          Abi.linuxArm64: path.join(
-            rootDir,
-            'target',
-            'aarch64-unknown-linux-gnu',
-            'release',
-            binaryName,
-          ),
-          Abi.linuxX64: path.join(
-            rootDir,
-            'target',
-            'x86_64-unknown-linux-gnu',
-            'release',
-            binaryName,
-          ),
-          Abi.windowsX64: path.join(
-            rootDir,
-            'target',
-            'x86_64-pc-windows-msvc',
-            'release',
-            binaryName,
-          ),
-        },
-      );
+      try {
+        Isar.initializeIsarCore(
+          libraries: {
+            Abi.macosArm64: path.join(
+              rootDir,
+              'target',
+              'aarch64-apple-darwin',
+              'release',
+              binaryName,
+            ),
+            Abi.macosX64: path.join(
+              rootDir,
+              'target',
+              'x86_64-apple-darwin',
+              'release',
+              binaryName,
+            ),
+            Abi.linuxArm64: path.join(
+              rootDir,
+              'target',
+              'aarch64-unknown-linux-gnu',
+              'release',
+              binaryName,
+            ),
+            Abi.linuxX64: path.join(
+              rootDir,
+              'target',
+              'x86_64-unknown-linux-gnu',
+              'release',
+              binaryName,
+            ),
+            Abi.windowsX64: path.join(
+              rootDir,
+              'target',
+              'x86_64-pc-windows-msvc',
+              'release',
+              binaryName,
+            ),
+          },
+        );
+      } catch (_) {}
     }
     _setUp = true;
   }
