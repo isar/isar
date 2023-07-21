e3a2a50530fc6c5aa10570d4e59024bbd918c09c
Simon Choi <13610195+simc@users.noreply.github.com>
2023-07-21 20:54:06 +0200

    Fixes

--- packages/isar_test/tool/generate_long_double_test.dart
+++ packages/isar_test/tool/generate_long_double_test.dart
@@ -38,43 +38,4 @@ void main() {
       .replaceAll('floatModels', 'doubleModels');
   File('test/filter/filter_double_list_test.dart')
       .writeAsStringSync(doubleListTest);
-
-// where clauses
-  final whereIntTestFile = File('test/index/where_int_test.dart');
-  final whereIntTest = whereIntTestFile.readAsStringSync();
-  final whereLongTest = whereIntTest
-      .replaceAll('Int', 'Long')
-      .replaceAll('short', 'int')
-      .replaceAll('where_int_test', 'where_long_test')
-      .replaceAll('intModels', 'longModels');
-  File('test/index/where_long_test.dart').writeAsStringSync(whereLongTest);
-
-  /*final whereIntListTestFile = File('test/index/where_int_list_test.dart');
-  final whereIntListTest = whereIntListTestFile.readAsStringSync();
-  final whereLongListTest = whereIntListTest
-      .replaceAll('Int', 'Long')
-      .replaceAll('short', 'int')
-      .replaceAll('where_int_test', 'where_long_test')
-      .replaceAll('intModels', 'longModels');
-  File('test/index/where_long_list_test.dart')
-      .writeAsStringSync(whereLongListTest);*/
-
-  final whereFloatTestFile = File('test/index/where_float_test.dart');
-  final whereFloatTest = whereFloatTestFile.readAsStringSync();
-  final whereDoubleTest = whereFloatTest
-      .replaceAll('Float', 'Double')
-      .replaceAll('float', 'double')
-      .replaceAll('where_float_test', 'where_double_test')
-      .replaceAll('floatModels', 'doubleModels');
-  File('test/index/where_double_test.dart').writeAsStringSync(whereDoubleTest);
-
-  final whereFloatListTestFile = File('test/index/where_float_list_test.dart');
-  final whereFloatListTest = whereFloatListTestFile.readAsStringSync();
-  final whereDoubleListTest = whereFloatListTest
-      .replaceAll('Float', 'Double')
-      .replaceAll('float', 'double')
-      .replaceAll('where_float_test', 'where_double_test')
-      .replaceAll('floatModels', 'doubleModels');
-  File('test/index/where_double_list_test.dart')
-      .writeAsStringSync(whereDoubleListTest);
 }
