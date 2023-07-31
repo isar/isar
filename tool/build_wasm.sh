773cd099286ede59e49321bf49a920bb95fa419a
Simon Choi <13610195+simc@users.noreply.github.com>
2023-07-31 02:56:40 +0200

    Update build_wasm.sh

--- tool/build_wasm.sh
+++ tool/build_wasm.sh
@@ -1,3 +1,3 @@
-rustup target add target wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown
 cargo build --target wasm32-unknown-unknown --features sqlite --no-default-features -p isar --release
 mv "target/wasm32-unknown-unknown/release/isar.wasm" "isar.wasm"
