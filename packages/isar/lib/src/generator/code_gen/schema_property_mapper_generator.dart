9d3a8332a8ba0f60c7450680186ec4c676e2fb5f
Tomás Weigenast <tomaswegenast@gmail.com>
2023-09-02 11:36:18 -0300

    index start from 1 instead from 0

--- packages/isar/lib/src/generator/code_gen/schema_property_mapper_generator.dart
+++ packages/isar/lib/src/generator/code_gen/schema_property_mapper_generator.dart
@@ -1,7 +1,7 @@
 part of isar_generator;
 
 String _generatePropertyMapper(ObjectInfo object) {
-  var index = 0;
+  var index = 1;
   return '''
     const ${object.dartName.capitalize()}PropertyMapper = <String, int>{
       ${object.properties.map((e) => '"${e.dartName}":${index++}').join(",")}
