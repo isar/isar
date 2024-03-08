9f7eceda779b31033567c5afa86067a568b66ec5
Martin Clauss <mc@sngs.de>
2024-03-08 16:04:51 +0100

    Fix windows release build

--- tool/build_windows.sh
+++ tool/build_windows.sh
@@ -1,4 +1,5 @@
 if [ "$1" = "x64" ]; then
+  export PATH=/c/mingw64/bin:$PATH # Some bastard installer removes this from path during preparation
   rustup target add x86_64-pc-windows-msvc
   cargo build --target x86_64-pc-windows-msvc --features sqlcipher-vendored --release
   mv "target/x86_64-pc-windows-msvc/release/isar.dll" "isar_windows_x64.dll"
