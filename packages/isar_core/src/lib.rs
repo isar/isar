d0bad36aae79e451c33b3e15c094f4f836d4ef4e
Simon Choi <13610195+simc@users.noreply.github.com>
2025-02-02 22:36:47 +0100

    Update lib.rs

--- packages/isar_core/src/lib.rs
+++ packages/isar_core/src/lib.rs
@@ -1,6 +1,4 @@
 #![allow(clippy::new_without_default)]
-#![feature(float_next_up_down)]
-#![feature(lazy_cell)]
 
 #[cfg(not(target_endian = "little"))]
 compile_error!("Only little endian systems are supported.");
