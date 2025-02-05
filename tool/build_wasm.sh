fefbd13105e2e3b20adbc3b9cc406bb755cfec93
Simon Choi <13610195+simc@users.noreply.github.com>
2025-02-05 15:52:05 +0100

    fix

--- tool/build_wasm.sh
+++ tool/build_wasm.sh
@@ -1,4 +1,4 @@
 rustup target add wasm32-unknown-unknown
 cargo install wasm-opt
-cargo build --target wasm32-unknown-unknown --features sqlite --no-default-features -p isar --release
-mv target/wasm32-unknown-unknown/release/isar.wasm isar.wasm
+cargo build --target wasm32-unknown-unknown --features sqlite --no-default-features -p isar
+mv target/wasm32-unknown-unknown/debug/isar.wasm isar.wasm
